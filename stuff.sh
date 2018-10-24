b12a92d7b10ec21fb5e17118853bcaa58e3bf091
Danny Roberts <danzi@ury.york.ac.uk>
2018-10-24 01:50:27 +0100

    Fix linebreaks

--- stuff.sh
+++ stuff.sh
@@ -1,5 +1,4 @@
 #!bin/sh
 #cat /mnt/pool0/test
 #ping ury
-cat /music/test || echo "Jukebox SHOULD failover cleanly. CHECK LIVE FEED." | mail -s "NFS mount has dropped" 
-danzibob@gmail.com il598@york.ac.uk 
+cat /music/test || echo "Jukebox SHOULD failover cleanly. CHECK LIVE FEED." | mail -s "NFS mount has dropped" danzibob@gmail.com il598@york.ac.uk 
